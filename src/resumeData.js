f2e1c7c64055dcb7c5787edb5106527ac2ab94d9
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-07-13 21:40:11 -0400

    add

---
+++ src/resumeData.js
@@ -0,0 +1,109 @@
+let resumeData = {
+  imagebaseurl: "https://www.linkedin.com/in/mehulndoshi/",
+  name: "Intensify Solutions",
+  role:
+    "Highly customizable with seamless integration and scalable to enhance growth and agility in your core business performance",
+  linkedinId: "www.linkedin.com/in/mehulndoshi/",
+  skypeid: "Your skypeid",
+  roleDescription:
+    " ",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/mehulndoshi/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/Amitabh-K",
+      className: "fa fa-github",
+    },
+    {
+      name: "skype",
+      url: "https://twitter.com/amitabhkumar2",
+      className: "fa fa-twitter",
+    },
+  ],
+  aboutme:
+    "9+ years managing transformation of finance functions in an outsourced and shared services environment",
+  address: "India",
+  website: "https://intensifysolutions.com/",
+  education: [
+    {
+      UniversityName: "Pune University",
+      specialization: "Some specialization",
+      MonthOfPassing: "Aug",
+      YearOfPassing: "2020",
+      Achievements: "Some Achievements",
+    },
+    {
+      UniversityName: "Some University",
+      specialization: "Some specialization",
+      MonthOfPassing: "Jan",
+      YearOfPassing: "2018",
+      Achievements: "Some Achievements",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Some Company",
+      specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "Some Achievements",
+    },
+    {
+      CompanyName: "Some Company",
+      specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "Some Achievements",
+    },
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "Reactjs",
+    },
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+  ],
+};
+
+export default resumeData;
